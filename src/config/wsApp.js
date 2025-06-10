2664a09bcfe7d6effc15378a1a9fe2a9d2ef82c1
LRamdhan <luzirmdhn@gmail.com>
2025-06-10 18:54:52 +0700

    fix: socket.io

--- src/config/wsApp.js
+++ src/config/wsApp.js
@@ -9,20 +9,20 @@ const attachWebSocket = (server) => {
   wsApp = new Server(server, {
     cors: {
       origin: "*",
-      methods: ['GET', 'POST'],
-      credentials: true
+      // methods: ['GET', 'POST'],
+      // credentials: true
     },
     connectionStateRecovery: {}
   });
   
   // middleware check connect client
-  wsApp.use(checkConnectClient);
+  // wsApp.use(checkConnectClient);
 
   // driver ws instance
   const wsServerDriver = wsApp.of('/driver');
 
   // middleware check connect driver
-  wsServerDriver.use(checkConnectDriver);
+  // wsServerDriver.use(checkConnectDriver);
 
   // driver listener
   wsServerDriver.on('connection', driverWsListener);
